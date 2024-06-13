8a721b90357713b96cc3ab4cd7f1ffcbb3e51754
ZEZE | ぜ-ぜ <166899915+luhzeze@users.noreply.github.com>
2024-06-13 19:41:03 +0600

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        float a = sc.nextFloat();
+        System.out.println("Введите операцию (+, -, *, /)");
+        char operation = sc.next().charAt(0);
+        System.out.println("Введите второе число:");
+        float b = sc.nextFloat();
+
+        if (operation == '+') {
+            float result = a + b;
+            System.out.println(a + " + " + b + " = " + result);
+        } else if (operation == '-') {
+            float result = a - b;
+            System.out.println(a + " - " + b + " = " + result);
+        } else if (operation == '*') {
+            float result = a * b;
+            System.out.println(a + " * " + b + " = " + result);
+        } else if (operation == '/') {
+            float result = a / b;
+            System.out.println(a + " / " + b + " = " + result);
+        } else {
+            System.out.println("Указанная операция не поддерживается калькулятором");
+        }
+    }
+}
